9151bef6077d07a3dbc4529103aaf42389bce04a
zharkovstas <zharkovstas@skbkontur.ru>
2015-01-19 23:19:38 +0500

    Фикс переноса в отдельную папку

--- Orchestra.Site/Properties/AssemblyInfo.cs
+++ Orchestra.Site/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Orchestra")]
+[assembly: AssemblyTitle("Orchestra.Site")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Orchestra")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Orchestra.Site")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("af607a24-2a3d-46ce-a48f-e873cf4bd830")]
+[assembly: Guid("2ef90ee4-69c7-46a9-ae41-b6822e8f9417")]
 
 // Version information for an assembly consists of the following four values:
 //
